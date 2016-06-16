779a20e045e9226cf57cb706b84938adf97a672c
weilifeng <lifeng.wei@zhenai.com>
2016-06-16 14:30:14 +0800

    add desktop notify

---
+++ src/main/java/cc/hunter/killua/constants/MachineConstants.java
@@ -0,0 +1,16 @@
+package cc.hunter.killua.constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/6/16.
+ */
+public class MachineConstants {
+
+    public static final String[] ips = new String[] {};
+    public static final String[] projects = new String[]{};
+
+    public static Map<String, String> userInfos = new HashMap<>();
+
+}
